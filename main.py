452ce1cf8987fe5ca941c6a2e7bec767bee8da4b
3 <haoshaun@gmail.com>
2019-09-13 17:08:26 +0800

    sigh

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence(discord.Game(name="with your goshes"))
+
 
 keep_alive()
-#client.loop.create_task(my_background_task())
+client.loop.create_task(my_background_task())
 client.run(token)
